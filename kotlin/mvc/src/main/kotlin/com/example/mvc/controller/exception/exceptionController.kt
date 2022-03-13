ee7dd29410fd5c712143dc5fa672e7fe1960bc32
youngyoung0 <taeyoung6873@gmail.com>
2022-03-13 23:03:42 +0900

    Kotlin : ExceptionHandler -2
    ExceptionHandler를 활용하여 자세한 에러 정보 클라이언트에게 보여주기

--- kotlin/mvc/src/main/kotlin/com/example/mvc/controller/exception/exceptionController.kt
+++ kotlin/mvc/src/main/kotlin/com/example/mvc/controller/exception/exceptionController.kt
@@ -1,17 +1,17 @@
 package com.example.mvc.controller.exception
 
 import com.example.mvc.model.http.ErrorResponse
+import com.example.mvc.model.http.UserRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.*
 import java.time.LocalDateTime
 import javax.servlet.http.HttpServletRequest
 import javax.validation.ConstraintViolationException
+import javax.validation.Valid
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
@@ -40,6 +40,39 @@ class exceptionController {
         return name+ " " + age
     }
 
+    @PostMapping("")
+    fun post(@Valid @RequestBody userRequest: UserRequest): UserRequest {
+        println(userRequest)
+        return userRequest
+    }
+
+    @ExceptionHandler(value=[MethodArgumentNotValidException::class])
+    fun methodArgumentNotValidException(e: MethodArgumentNotValidException, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+        val errors = mutableListOf<com.example.mvc.model.http.Error>()
+
+        e.bindingResult.allErrors.forEach{ errorObject->
+            val error = com.example.mvc.model.http.Error().apply{
+                this.field = (errorObject as FieldError).field
+                this.message = errorObject.defaultMessage
+                this.value = errorObject.rejectedValue
+            }
+            errors.add(error)
+        }
+        // 2. ErrorResponse
+        val errorResponse = ErrorResponse().apply {
+            this.resultCode = "FAIL"
+            this.httpStatus = HttpStatus.BAD_REQUEST.value().toString()
+            this.httpMethod=request.method
+            this.message = "요청에 에러가 발생하였습니다."
+            this.path = request.requestURI.toString()
+            this.timestamp = LocalDateTime.now()
+            this.errors= errors
+        }
+
+        // 3. ResponseEntity
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse)
+    }
+
     @ExceptionHandler(value=[ConstraintViolationException::class])
     fun constraintViolationException(e: ConstraintViolationException, request: HttpServletRequest): ResponseEntity<ErrorResponse>{
         // 1. 에러 분석
@@ -50,7 +83,8 @@ class exceptionController {
             val message = it.message
             val error = com.example.mvc.model.http.Error().apply{
                 this.field = field
-                this.message = message
+                this.message = it.message
+                this.value=it.invalidValue
             }
             errors.add(error)
         }
@@ -59,6 +93,7 @@ class exceptionController {
         val errorResponse = ErrorResponse().apply {
             this.resultCode = "FAIL"
             this.httpStatus = HttpStatus.BAD_REQUEST.value().toString()
+            this.httpMethod=request.method
             this.message = "요청에 에러가 발생하였습니다."
             this.path = request.requestURI.toString()
             this.timestamp = LocalDateTime.now()
--- kotlin/mvc/src/main/kotlin/com/example/mvc/model/http/ErrorResponse.kt
+++ kotlin/mvc/src/main/kotlin/com/example/mvc/model/http/ErrorResponse.kt
@@ -10,6 +10,9 @@ data class ErrorResponse (
     @field:JsonProperty("http_status")
     var httpStatus:String?=null,
 
+    @field:JsonProperty("http_method")
+    var httpMethod:String?=null,
+
     var message:String ?=null,
     var path:String?=null,
     var timestamp: LocalDateTime?=null,
@@ -18,7 +21,8 @@ data class ErrorResponse (
 
 data class Error(
     var field:String?=null,
-    var message: String?=null
+    var message: String?=null,
+    var value: Any?=null
 )
 
 
