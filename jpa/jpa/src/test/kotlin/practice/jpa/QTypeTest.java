4d9d0da3d5dcff909728c5024c53f5d512a2f43a
youngyoung0 <taeyoung6873@gmail.com>
2022-03-22 01:26:40 +0900

    검색 조건 쿼리

--- jpa/jpa/src/test/kotlin/practice/jpa/QTypeTest.java
+++ jpa/jpa/src/test/kotlin/practice/jpa/QTypeTest.java
@@ -7,6 +7,7 @@ import practice.jpa.domain.Member;
 import practice.jpa.domain.QMember;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static practice.jpa.domain.QMember.member;
 
 @SpringBootTest
 public class QTypeTest {
@@ -14,7 +15,7 @@ public class QTypeTest {
 
     @Test
     public void startQuerydsl(){
-        QMember m = QMember.member;
+        QMember m = member;
         Member findMember=  queryFactory
                 .select(m)
                 .from(m)
@@ -23,5 +24,43 @@ public class QTypeTest {
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
-    
+
+    @Test
+    public void search(){
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+        // member.userName.eq("member1")
+        // member.userName.ne("member1")
+        // member.username.eq("member1").not -> username != 'member1'
+        // member.username.isNotnull() //이름이 is not null
+        // member.age.in(10,20) age in (10,20)
+        // member.age.notIn(10,20) age not in (10, 20)
+        // member.age.between(10,30) // between 10, 30
+
+        // member.age.goe(30) age>=30
+        // member.age.get(30) age>30
+        // member.age.loe(30 age<=30
+        // member.age.lt(30) age<30
+
+        // member.username.like("member%") like검색
+        // member.username.contains("member") // like '%like%' 검색
+        // member.username.startsWith("member") // like 'meber%'검색
+
+    }
+
+    @Test
+    public void searchAndParam(){
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                        .and(member.age.eq(10)))
+                .fetchOne();
+
 }
