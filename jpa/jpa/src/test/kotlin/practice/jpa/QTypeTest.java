3eae1efb81185969d65fecde367b1b2e860cc7cb
youngyoung0 <taeyoung6873@gmail.com>
2022-03-22 01:27:18 +0900

    .

--- jpa/jpa/src/test/kotlin/practice/jpa/QTypeTest.java
+++ jpa/jpa/src/test/kotlin/practice/jpa/QTypeTest.java
@@ -54,13 +54,13 @@ public class QTypeTest {
     }
 
     @Test
-    public void searchAndParam(){
+    public void searchAndParam() {
         Member findMember = queryFactory
                 .selectFrom(member)
                 .where(
                         member.username.eq("member1"),
                         member.age.eq(10)
-                        .and(member.age.eq(10)))
+                                .and(member.age.eq(10)))
                 .fetchOne();
-
+    }
 }
