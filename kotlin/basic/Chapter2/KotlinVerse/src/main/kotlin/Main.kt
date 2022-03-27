4bc32adcfbb046ab8ad600b7f66b1ba0d80ee911
youngyoung0 <taeyoung6873@gmail.com>
2022-03-28 00:36:22 +0900

    Kotlin : 기본문법 정리

--- kotlin/basic/Chapter2/KotlinVerse/src/main/kotlin/Main.kt
+++ kotlin/basic/Chapter2/KotlinVerse/src/main/kotlin/Main.kt
@@ -1,4 +1,12 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-    println("Hello KotlinVerse!")
+    var a =1
+    println(a++)
+    println(++a)
+    println(--a)
+    println(a--)
+
+    var result = 3
+    result *=10
+    result +=6
+    println(result)
 }
