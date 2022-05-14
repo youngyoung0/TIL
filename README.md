e57ca30370d0eebfb221815d4d604cbe4d2c8e2b
youngyoung0 <taeyoung6873@gmail.com>
2022-05-14 18:12:19 +0900

    java_playground: 간단한 구구단 구현

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 - JPA
 - Kotlin
+- Java
---
+++ java/java playground/javaBasic/src/Gugudan.java
@@ -0,0 +1,12 @@
+public class Gugudan {
+    public static void main(String[] args){
+
+        for(int i = 2; i<10; i++){
+            System.out.println(i+" 단입니다.");
+            for(int j = 1; j<10; j++){
+                System.out.println(i * j);
+            }
+
+        }
+    }
+}
