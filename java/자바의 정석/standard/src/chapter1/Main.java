21adf9ecf2c1b8212495f497c09f67e55cb7c9a2
youngyoung0 <taeyoung6873@gmail.com>
2022-11-14 23:34:39 +0900

    standard_chapter1 : Main method

---
+++ java/자바의 정석/standard/src/chapter1/Main.java
@@ -0,0 +1,8 @@
+package src.chapter1;
+
+public class Main {
+    // 프로그램을 실행할때 'java.exe'에 의해 호출될 수 있도록 약속된 부분이므로 항상 똑같이 적어주어야 한다.
+    public static void main (String[] args){ // main 메서드의 선언부이다.
+        System.out.println("hello world");
+    }
+}
