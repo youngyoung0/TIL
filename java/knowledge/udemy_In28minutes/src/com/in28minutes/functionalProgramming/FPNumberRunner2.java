6fb81f06006ff9e81df789a386e6d67135493de7
youngyoung0 <taeyoung6873@gmail.com>
2022-12-08 02:15:30 +0900

    In28Minutes : FunctionalProgramming - 최소값, 리스트에 모으기

--- java/knowledge/udemy_In28minutes/src/com/in28minutes/functionalProgramming/FPNumberRunner2.java
+++ java/knowledge/udemy_In28minutes/src/com/in28minutes/functionalProgramming/FPNumberRunner2.java
@@ -1,6 +1,8 @@
 package com.in28minutes.functionalProgramming;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FPNumberRunner2 {
@@ -18,5 +20,14 @@ public class FPNumberRunner2 {
 
         // max
         System.out.println(List.of(23,12,34,53).stream().max((n1, n2 )-> Integer.compare(n1, n2)).get());
+
+        // min
+        List.of(23,12,34,53).stream().filter(e -> e%2 == 1).forEach(e -> System.out.println(e));
+
+        System.out.println(List.of(23,12,34,53).stream().filter(e -> e%2 == 1).collect(Collectors.toList()));
+
+        // .boxed - int, long, double 요소를 Integer, Long, Double 요소로 박싱해서 stream 생성
+        // .collect - Stream의 데이터 변형 등의 처리를 하고 원하는 자료형을 변환
+        System.out.println(IntStream.range(1,11).map(e -> e * e).boxed().collect(Collectors.toList()));
     }
 }
