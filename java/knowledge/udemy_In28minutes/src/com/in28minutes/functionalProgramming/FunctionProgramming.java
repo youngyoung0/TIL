1c6c830b634fe2d47843b08628cb0fe61a35f8a3
youngyoung0 <taeyoung6873@gmail.com>
2022-12-08 01:47:17 +0900

    In28Minutes : FunctionalProgramming - Sort, Distinct, Fitter, Map

--- java/knowledge/udemy_In28minutes/src/com/in28minutes/functionalProgramming/FunctionProgramming.java
+++ java/knowledge/udemy_In28minutes/src/com/in28minutes/functionalProgramming/FunctionProgramming.java
@@ -6,19 +6,28 @@ public class FunctionProgramming {
     public static void main (String[] args){
         List<String> list = List.of("Apple", "Bat", "Cat", "DOG");
         List<Integer> numberList = List.of(1,2,3,4,5,6,7,8,9);
+        List<Integer> mishmashNumberList = List.of(3,5,8,213,45,4,7);
+        List<Integer> distinctNumberList = List.of(3,5,3,5,213,45,4,7);
 
 
-        System.out.println("printBasicWithFiltering");
-        printBasicWithFiltering(list);
-        System.out.println("printWithFP");
-        printWithFP(list);
-        System.out.println("printWithFPWithFiltering");
-        printWithFPWithFiltering(list);
-        System.out.println("printOddNumber");
-        printOddNumber(numberList);
-        System.out.println("printEvenNumber");
-        printEvenNumber(numberList);
-
+//        System.out.println("printBasicWithFiltering");
+//        printBasicWithFiltering(list);
+//        System.out.println("printWithFP");
+//        printWithFP(list);
+//        System.out.println("printWithFPWithFiltering");
+//        printWithFPWithFiltering(list);
+//        System.out.println("printOddNumber");
+//        printOddNumber(numberList);
+//        System.out.println("printEvenNumber");
+//        printEvenNumber(numberList);
+//        System.out.println("fpSum");
+//        System.out.println(fpSum(numberList));
+        System.out.println("fpSort");
+        fpSort(mishmashNumberList);
+        System.out.println("fpdDistinct");
+        fpdDistinct(distinctNumberList);
+        System.out.println("fpMap");
+        fpMap(distinctNumberList);
     }
 
     private static void printBasic(List<String> list){
@@ -61,4 +70,30 @@ public class FunctionProgramming {
                         element -> System.out.println(element)
                 );
     }
+
+    // 람다 모든 수 덧셈
+    private static int fpSum(List<Integer> numbers){
+        return numbers.stream().reduce(0,
+                (number1, number2)->  number1 + number2);
+    }
+
+    // 람다 정렬
+    private static void fpSort(List<Integer> numbers){
+        numbers.stream().sorted().forEach(
+                number -> System.out.println(number)
+        );
+    }
+
+    // 람다 중복 제거
+    private static void fpdDistinct(List<Integer> numbers){
+        numbers.stream().distinct().sorted().forEach(
+                number -> System.out.println(number)
+        );
+    }
+
+    // map
+    private static  void fpMap(List<Integer>numbers){
+        numbers.stream().distinct().map(number -> number * number).forEach(number -> System.out.println(number));
+    }
+
 }
