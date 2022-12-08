eab03e8f107084273d6fc68e274648d49e83a446
taylor <77282011+youngyoung0@users.noreply.github.com>
2022-12-09 03:18:59 +0900

    In28Minutes : FunctionalPrograming - conclusion

--- java/knowledge/udemy_In28minutes/src/com/in28minutes/lambda/LambdaBehindTheScreensRunner.java
+++ java/knowledge/udemy_In28minutes/src/com/in28minutes/lambda/LambdaBehindTheScreensRunner.java
@@ -33,6 +33,7 @@ public class LambdaBehindTheScreensRunner {
 
     public static void main(String[] args){
 
+        // 1.
         List.of(23,43,34,45).stream()
                 .filter(n -> n%2 == 0)
                 .map(n -> n * n)
@@ -42,17 +43,35 @@ public class LambdaBehindTheScreensRunner {
                 .filter(new EvenNumberPredicate())
                 .map(new NumberSquareMapper())
                 .forEach(new SystemOutConsumer());
+        // .map
+        //  R apply(T t);
+        // <R> Stream<R> map(Function<? super T, ? extends R> mapper);
+
+        // .filter
+        // Stream<T> filter(Predicate<? super T> predicate);
+        // boolean test(T t);
+
+        // .forEach
+        // void forEach(Consumer<? super T> action);
+        // void accept(T t);
+
+        System.out.println("-----------------");
+        // 2.
+        // 변수에 함수를 저장
+        // 함수를 메소드로 전달
+        // 메서드에서 함수를 반환
+        Predicate<? super Integer> evenPredicate = createEvenPredicate();
+        Predicate<? super Integer> oddPredicate = number -> number % 2 == 1;
+        Function<? super Integer, ?> squareFunction = number -> number * number;
+        Consumer<?> systemConsumer = System.out::println;
+
+        List.of(23,43,34,45).stream()
+                .filter(evenPredicate)
+                .map(n -> n * n)
+                .forEach(System.out::println);
+
     }
-
-    // .map
-    //  R apply(T t);
-    // <R> Stream<R> map(Function<? super T, ? extends R> mapper);
-
-    // .filter
-    // Stream<T> filter(Predicate<? super T> predicate);
-    // boolean test(T t);
-
-    // .forEach
-    // void forEach(Consumer<? super T> action);
-    // void accept(T t);
+    private static Predicate<? super Integer> createEvenPredicate(){
+        return  n -> n % 2 == 0;
+    }
 }
