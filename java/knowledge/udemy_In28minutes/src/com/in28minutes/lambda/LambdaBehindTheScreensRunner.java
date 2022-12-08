2bc1ebfff9cc52d0110db7a8b199da8914c23db0
taylor <77282011+youngyoung0@users.noreply.github.com>
2022-12-09 02:51:17 +0900

    In28Minutes : FunctionalPrograming - function behind

---
+++ java/knowledge/udemy_In28minutes/src/com/in28minutes/lambda/LambdaBehindTheScreensRunner.java
@@ -0,0 +1,58 @@
+package com.in28minutes.lambda;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+class EvenNumberPredicate implements Predicate<Integer> {
+
+    @Override
+    public boolean test(Integer number) {
+        return number % 2 == 0;
+    }
+}
+
+class SystemOutConsumer implements Consumer<Integer> {
+
+    @Override
+    public void accept(Integer number) {
+        System.out.println(number);
+    }
+}
+
+class NumberSquareMapper implements Function<Integer, Integer>{
+
+    @Override
+    public Integer apply(Integer integer) {
+        return integer * integer;
+    }
+}
+
+public class LambdaBehindTheScreensRunner {
+
+    public static void main(String[] args){
+
+        List.of(23,43,34,45).stream()
+                .filter(n -> n%2 == 0)
+                .map(n -> n * n)
+                .forEach(n -> System.out.println(n));
+
+        List.of(23,43,34,45).stream()
+                .filter(new EvenNumberPredicate())
+                .map(new NumberSquareMapper())
+                .forEach(new SystemOutConsumer());
+    }
+
+    // .map
+    //  R apply(T t);
+    // <R> Stream<R> map(Function<? super T, ? extends R> mapper);
+
+    // .filter
+    // Stream<T> filter(Predicate<? super T> predicate);
+    // boolean test(T t);
+
+    // .forEach
+    // void forEach(Consumer<? super T> action);
+    // void accept(T t);
+}
