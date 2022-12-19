60c10eb1a76473241f9eab0b7aab68800a29010a
youngyoung0 <taeyoung6873@gmail.com>
2022-12-20 01:22:42 +0900

    Many to Many

---
+++ jpa/guru/jpa/src/main/java/guru/jpa/domain/Author.java
@@ -0,0 +1,23 @@
+package guru.jpa.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    public Author(){
+    }
+
+}
