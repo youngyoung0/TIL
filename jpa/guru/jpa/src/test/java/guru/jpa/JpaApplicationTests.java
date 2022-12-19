fdf27882901f25addba737b8fc7f824bbfa2574c
youngyoung0 <taeyoung6873@gmail.com>
2022-12-20 01:08:38 +0900

    init

---
+++ jpa/guru/jpa/src/test/java/guru/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
