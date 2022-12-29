de3a34a55bc5b19146f2bd15446e29689c6e5ea0
youngyoung0 <taeyoung6873@gmail.com>
2022-12-30 00:56:50 +0900

    next-step: StringCalculator 리팩토링 - 2

--- java/nextStep/nextStep-junit/src/step2/StringCalculator.java
+++ java/nextStep/nextStep-junit/src/step2/StringCalculator.java
@@ -2,17 +2,36 @@ package step2;
 
 public class StringCalculator {
     public int add(String text) {
-        if (text == null || text.isEmpty()) {
+        if (isBlank(text)) {
             return 0;
         }
+        return sum(toInts(split(text)));
+    }
 
+    private boolean isBlank(String text) {
+        return text == null || text.isEmpty();
+    }
 
+    private String[] split(String text) {
         String[] values = text.split(",");
+        return values;
+    }
+
+    private int[] toInts(String[] values){
+        int[] numbers = new int[values.length];
+        for(int i =0; i< values.length; i++){
+            numbers[i] = Integer.parseInt(values[i]);
+        }
+        return numbers;
+    }
+
+    private int sum(int[] numbers) {
         int sum = 0;
-        for (String value : values) {
-            sum += Integer.parseInt(value);
+        for (int number : numbers) {
+            sum += number;
         }
         return sum;
-
     }
+
+
 }
