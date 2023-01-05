a2e91a2175d51239e30717c45c412239cd3b5d59
youngyoung0 <taeyoung6873@gmail.com>
2023-01-06 01:53:06 +0900

    TDD - 8장 객체 만들기

--- java/TDD/src/domain/Dollar.java
+++ java/TDD/src/domain/Dollar.java
@@ -1,9 +1,13 @@
 package domain;
 
 public class Dollar extends Money{
+    static Dollar dollar(int amount){
+        return new Dollar(amount);
+    }
     public Dollar(int amount){
         this.amount = amount;
     }
+
     Dollar times(int multiplier){
         return new Dollar(amount * multiplier);
     }
--- java/TDD/src/domain/Money.java
+++ java/TDD/src/domain/Money.java
@@ -1,7 +1,17 @@
 package domain;
 
-public class Money {
+abstract class Money {
     protected int amount;
+
+    static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount){
+        return new Franc(amount);
+    }
+
+    abstract Money times(int multiplier);
 
     public boolean equals(Object object){
         Money money = (Money) object;
