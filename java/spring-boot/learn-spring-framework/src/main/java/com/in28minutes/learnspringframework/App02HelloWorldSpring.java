6316ac512873227241104fa67be8134deef8ba77
youngyoung0 <taeyoung6873@gmail.com>
2023-01-31 00:57:05 +0900

    [spring-boot] 1. 빈에 대한 사용자 지정 이름을 구성할 수 있다는 사실 2. 스프링 컨텍스트에서 빈을 여러번 검색할 수 있다는 사실  3. Spring에서 관리하는 기존 빈을 사용할 수 있다는 것

--- java/spring-boot/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ java/spring-boot/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -1,8 +1,10 @@
 package com.in28minutes.learnspringframework;
 
+import com.in28minutes.learnspringframework.entity.Address;
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.PacmanGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 public class App02HelloWorldSpring {
     public static void main(String[] args) {
@@ -16,6 +18,14 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("name"));
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
-        System.out.println(context.getBean("address"));
+        System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("Person3Parameters"));
+        System.out.println(context.getBean("address2"));
+//        System.out.println(context.getBean(Address.class));
+
+        /* 1. 빈에 대한 사용자 지정 이름을 구성할 수 있다는 사실
+        *  2. 스프링 컨텍스트에서 빈을 여러번 검색할 수 있다는 사실
+        *  3. Spring에서 관리하는 기존 빈을 사용할 수 있다는 것
+        */
     }
 }
--- java/spring-boot/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ java/spring-boot/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -20,11 +20,26 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person(){
-        return new Person("Ravi", 20);
+        return new Person("Ravi", 20, new Address("Main Street", "Utrecht"));
     }
 
     @Bean
+    public Person person2MethodCall(){
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person Person3Parameters(String name, int age, Address address3){
+        return new Person(name, age, address3);
+    }
+
+    @Bean(name="address2")
     public Address address(){
         return new Address("Baker Street", "London");
+    }
+
+    @Bean(name="address3")
+    public Address address3(){
+        return new Address("Motinagar", "Hyderabad");
     }
 }
