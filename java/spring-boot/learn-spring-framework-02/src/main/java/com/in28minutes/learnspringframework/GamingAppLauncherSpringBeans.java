7e38aa833dc34721b6321c25d99aa093471fcb87
youngyoung0 <taeyoung6873@gmail.com>
2023-02-14 01:23:09 +0900

    [spring-boot] java 객체 생성 및 관리를 위한 Spring

--- java/spring-boot/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/GamingAppLauncherSpringBeans.java
+++ java/spring-boot/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/GamingAppLauncherSpringBeans.java
@@ -1,16 +1,18 @@
 package com.in28minutes.learnspringframework;
 
-import com.in28minutes.learnspringframework.entity.Address;
-import com.in28minutes.learnspringframework.entity.Person;
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.GamingConsole;
-import com.in28minutes.learnspringframework.helloworld.HelloWorldConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan("com.in28minutes.learnspringframework.game")
+public class GamingAppLauncherSpringBeans {
 
-public class App03GamingSpringBeans {
     public static void main(String[] args) {
-        try(AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(GamingConfiguration.class)){
+        try(AnnotationConfigApplicationContext context =
+                    new AnnotationConfigApplicationContext(GamingAppLauncherSpringBeans.class)){
             context.getBean(GamingConsole.class).up();
             context.getBean(GameRunner.class).run();
         }
--- java/spring-boot/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
+++ java/spring-boot/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -1,8 +1,10 @@
 package com.in28minutes.learnspringframework.game;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class GameRunner {
 
-//   private MarioGame game;
     private final GamingConsole game;
     public GameRunner(GamingConsole game) {
         this.game = game;
