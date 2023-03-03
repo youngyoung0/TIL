5d33018b0b2a2a21afc181257b6376b5a0009a09
youngyoung0 <taeyoung6873@gmail.com>
2023-03-04 03:34:22 +0900

    spring - 회원 도메인과 리포지토리 생성

---
+++ spring/hello-spring/src/main/java/start/hellospring/repository/MemberMemberRepository.java
@@ -0,0 +1,34 @@
+package start.hellospring.repository;
+
+import start.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemberMemberRepository implements MemberRepository{
+
+    private static final Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
