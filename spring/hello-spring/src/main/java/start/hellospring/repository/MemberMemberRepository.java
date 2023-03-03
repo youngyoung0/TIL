c8780608bac64bec8b5244af2bdae194352ffe77
youngyoung0 <taeyoung6873@gmail.com>
2023-03-04 03:53:30 +0900

    spring - 회원 리포지토 테스트 케이스 작성

--- spring/hello-spring/src/main/java/start/hellospring/repository/MemberMemberRepository.java
+++ spring/hello-spring/src/main/java/start/hellospring/repository/MemberMemberRepository.java
@@ -31,4 +31,8 @@ public class MemberMemberRepository implements MemberRepository{
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
+
+    public void clearStore(){
+        store.clear();
+    }
 }
