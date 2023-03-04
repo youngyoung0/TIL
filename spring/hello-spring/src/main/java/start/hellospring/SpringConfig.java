143b954869ab4eb9dd1401243d7c66e73f147d8a
youngyoung0 <taeyoung6873@gmail.com>
2023-03-05 01:23:01 +0900

    spring - 스프링 JdbcTemplate

--- spring/hello-spring/src/main/java/start/hellospring/SpringConfig.java
+++ spring/hello-spring/src/main/java/start/hellospring/SpringConfig.java
@@ -2,12 +2,22 @@ package start.hellospring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import start.hellospring.repository.JdbcTemplateMemberRepository;
 import start.hellospring.repository.MemberRepository;
 import start.hellospring.repository.MemoryMemberRepository;
 import start.hellospring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -15,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
