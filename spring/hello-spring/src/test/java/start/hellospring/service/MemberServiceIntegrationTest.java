a9df8d14df5c6c7c4d6b18ebe4a4bd041c2fd936
youngyoung0 <taeyoung6873@gmail.com>
2023-03-05 00:17:21 +0900

    spring - 스프링 통합 테스트

---
+++ spring/hello-spring/src/test/java/start/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,48 @@
+package start.hellospring.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import start.hellospring.domain.Member;
+import start.hellospring.repository.MemberRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        assertThatThrownBy(() -> memberService.join(member2))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
--- spring/hello-spring/src/test/java/start/hellospring/service/MemberServiceTest.java
+++ spring/hello-spring/src/test/java/start/hellospring/service/MemberServiceTest.java
@@ -1,6 +1,5 @@
 package start.hellospring.service;
 
-import org.assertj.core.api.AbstractThrowableAssert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
