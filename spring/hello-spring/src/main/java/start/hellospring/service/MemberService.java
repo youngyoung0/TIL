17e2dfbcb2e346e2559bb728333cb7bf3c74670c
youngyoung0 <taeyoung6873@gmail.com>
2023-03-04 17:26:08 +0900

    spring - 회원 웹 기능 - 홈 화면 추가

--- spring/hello-spring/src/main/java/start/hellospring/service/MemberService.java
+++ spring/hello-spring/src/main/java/start/hellospring/service/MemberService.java
@@ -1,14 +1,12 @@
 package start.hellospring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import start.hellospring.domain.Member;
 import start.hellospring.repository.MemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
