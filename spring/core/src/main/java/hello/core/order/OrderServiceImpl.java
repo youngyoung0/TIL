66a00d67c7241d726941578f2089389f048d98ef
youngyoung0 <taeyoung6873@gmail.com>
2023-03-07 21:19:46 +0900

    spring core - 새로운 할인 정책 적용과 문제점

--- spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,7 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +9,12 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // OrderServiceImpl이 DiscoutPolicy 인터페이스 뿐만 아니라 FixDiscountPolicy인 구현체 클래스도 의존하고 있다. DIP 의존관계 역전 원칙 위반
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // FixDiscountPolicy를 RateDiscountPolicy로 변경하는 순간 OrderServiceimpl의 소스코드도 함께 변경해야 한다. OCP 개방 폐쇄 원칙 위반
+    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
