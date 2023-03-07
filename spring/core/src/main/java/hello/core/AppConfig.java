d2b887f43bc93cf6bf770915ef92ac60aa9d64d3
youngyoung0 <taeyoung6873@gmail.com>
2023-03-07 23:05:41 +0900

    spring core - AppConfig 리팩터링

--- spring/core/src/main/java/hello/core/AppConfig.java
+++ spring/core/src/main/java/hello/core/AppConfig.java
@@ -9,11 +9,19 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
