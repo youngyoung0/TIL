89e06d6e102e49a7c712bbad1be10f87de60df86
youngyoung0 <taeyoung6873@gmail.com>
2023-03-07 21:59:37 +0900

    spring core - 관심사의 분리

--- spring/core/src/main/java/hello/core/MemberApp.java
+++ spring/core/src/main/java/hello/core/MemberApp.java
@@ -3,16 +3,16 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member memberA = new Member(1L, "memberA", Grade.VIP);
         memberService.join(memberA);
 
         Member findMember = memberService.findMember(1L);
-        System.out.println("member = " + memberA.getName() );
+        System.out.println("new member = " + memberA.getName() );
         System.out.println("findMember = " + findMember.getName());
     }
 }
