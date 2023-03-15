77540a478fe5f32de5c2b42a7af55783a8b1c622
youngyoung0 <taeyoung6873@gmail.com>
2023-03-15 23:08:41 +0900

    spring core - 탐색 위치와 기본 스캔 대상

--- spring/core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ spring/core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.ArrayList;
+
 public class AutoAppConfigTest {
     @Test
     void basicScan(){
@@ -13,5 +15,44 @@ public class AutoAppConfigTest {
 
         MemberService memberService = ac.getBean(MemberService.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+    private int[] proceedTask (int[] progresses, int[] speeds){
+        int [] days = new int[progresses.length];
+        for(int i=0; i<progresses.length; i++){
+            int day = 0;
+            while(progresses[i] <100){
+                progresses[i] += speeds[i];
+                day++;
+            }
+            days[i] = day;
+        }
+        return days;
+    }
+
+    private ArrayList<Integer> deployOrder(int[] days){
+        int complate = days[0];
+        int count = 0;
+        ArrayList<Integer> countBox = new ArrayList();
+        for(int day : days){
+            if(day <= complate){
+                count ++;
+            }else{
+                countBox.add(count);
+                count = 1;
+                complate = day;
+            }
+        }
+        countBox.add(count);
+        return countBox;
+    }
+
+    private int[] arrayListToIntArray(ArrayList<Integer> countBox){
+        int size = 0;
+        int[] answer = new int[countBox.size()];
+        for(Integer temp : countBox){
+            answer[size++] = temp.intValue();
+        }
+        return answer;
     }
 }
