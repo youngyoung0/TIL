bd91150710d17dd1542b101882e70a2a91706e76
youngyoung0 <taeyoung6873@gmail.com>
2023-03-18 18:25:59 +0900

    spring core - 조회 빈이 2개 이상 문제 발생

--- spring/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ spring/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements  DiscountPolicy{
     private int discountFixAmount = 1000; // 1000원 할인
 
@@ -11,7 +13,8 @@ public class FixDiscountPolicy implements  DiscountPolicy{
     public int discount(Member member, int price) {
         if(member.getGrade() == Grade.VIP){
             return discountFixAmount;
+        }else{
+            return 0;
         }
-        return 0;
     }
 }
