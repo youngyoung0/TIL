f7aab61138de85607cd80619dfedcce3461f5b43
youngyoung0 <taeyoung6873@gmail.com>
2023-03-18 19:14:17 +0900

    spring core - 조회 빈이 2개 이상 문제 발생 - 애노테이션 직접 만들기

--- spring/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ spring/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,18 +1,18 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
 
-    private int discountPercent = 10;
     @Override
     public int discount(Member member, int price) {
         if(member.getGrade() == Grade.VIP){
+            int discountPercent = 10;
             return (price * discountPercent) / 100;
         }
         return 0;
