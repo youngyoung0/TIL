6fe4e8fdc9eb8c9f0ca6f93c811500e97cd8f72f
taylor <77282011+youngyoung0@users.noreply.github.com>
2023-03-21 16:20:06 +0900

    playground - gradle version 7.1로 변경

---
+++ spring/demo/src/main/java/com/example/demo/controller/MapController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.config.MapConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("map")
+public class MapController {
+
+    private final MapConfig mapConfig;
+
+    @GetMapping("")
+    public ResponseEntity<String> mapConfig(){
+        System.out.println(mapConfig);
+        return new ResponseEntity<>("mapConfig", HttpStatus.OK);
+    }
+}
