ec95e4c2b0ed63e302eacaca415ead8a01601ae3
youngyoung0 <taeyoung6873@gmail.com>
2023-04-08 09:45:22 +0900

    mvc2 valid - 오류코드와 메세지 처리4 (MessageCodesResolver)

---
+++ spring/mvc2/validation-start/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,36 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.ObjectError;
+
+public class MessageCodesResolverTest {
+
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject(){
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        for(String messageCode : messageCodes){
+            System.out.println("messagedCodes = " + messageCode);
+        }
+        Assertions.assertThat(messageCodes).containsExactly("required.item", "requried");
+    }
+
+    @Test
+    void messageCodesResolverField(){
+        String[] messageCodes = codesResolver.resolveMessageCodes("required","item", "itemName", String.class);
+        for(String messageCode : messageCodes){
+            System.out.println("messageCode = " + messageCode);
+        }
+        Assertions.assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+    }
+}
