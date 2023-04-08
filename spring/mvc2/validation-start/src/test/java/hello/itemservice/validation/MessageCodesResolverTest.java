f57a0b41ce6a7272c9364f4c38b477a4a652be31
youngyoung0 <taeyoung6873@gmail.com>
2023-04-08 10:03:56 +0900

    mvc2 valid - 오류코드와 메세지 처리5 (각 레벨별로 처리)

--- spring/mvc2/validation-start/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
+++ spring/mvc2/validation-start/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.validation.DefaultMessageCodesResolver;
 import org.springframework.validation.MessageCodesResolver;
-import org.springframework.validation.ObjectError;
 
 public class MessageCodesResolverTest {
 
