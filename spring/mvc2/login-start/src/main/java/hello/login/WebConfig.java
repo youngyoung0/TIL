e2416e97fd9ee8fde56dc32ee19d0082f703843d
youngyoung0 <taeyoung6873@gmail.com>
2023-04-13 18:26:24 +0900

    mvc2 interceptor - 요청 로그

--- spring/mvc2/login-start/src/main/java/hello/login/WebConfig.java
+++ spring/mvc2/login-start/src/main/java/hello/login/WebConfig.java
@@ -1,16 +1,28 @@
 package hello.login;
 
+import hello.login.web.Interceptor.LogInterceptor;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
-    @Bean
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico","/errorf");
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter(){
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<Filter>();
         filterRegistrationBean.setFilter(new LogFilter());
