47ff5a87050b24aea53adaa6644ffcaee612fcd9
youngyoung0 <taeyoung6873@gmail.com>
2023-04-14 14:56:18 +0900

    mvc2 errorPage - 오류화면 설정

--- spring/mvc2/exception/src/main/java/hello/exception/servlet/ServletExController.java
+++ spring/mvc2/exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -4,8 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import static javax.servlet.RequestDispatcher.*;
 
 @Slf4j
 @Controller
@@ -17,13 +20,30 @@ public class ServletExController {
 
 
     @GetMapping("error-404")
-    public void error404(HttpServletResponse response) throws IOException {
+    public void error404(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.sendError(404, "404 오류!");
+        printErrorInfo(request);
     }
 
     @GetMapping("error-500")
-    public void error500(HttpServletResponse response) throws IOException {
+    public void error500(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.sendError(500);
+        printErrorInfo(request);
+    }
+
+    private void printErrorInfo(HttpServletRequest request) {
+        log.info("ERROR_EXCEPTION: ex=",
+                request.getAttribute(ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE: {}",
+                request.getAttribute(ERROR_EXCEPTION_TYPE));
+        log.info("ERROR_MESSAGE: {}", request.getAttribute(ERROR_MESSAGE)); // ex의 경우 NestedServletException 스프링이 한번 감싸서 반환
+        log.info("ERROR_REQUEST_URI: {}",
+                request.getAttribute(ERROR_REQUEST_URI));
+        log.info("ERROR_SERVLET_NAME: {}",
+                request.getAttribute(ERROR_SERVLET_NAME));
+        log.info("ERROR_STATUS_CODE: {}",
+                request.getAttribute(ERROR_STATUS_CODE));
+        log.info("dispatchType={}", request.getDispatcherType());
     }
 
 }
