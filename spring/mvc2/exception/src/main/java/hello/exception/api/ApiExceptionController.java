39fea8ef28a84710f450707f53c054c62e2e99ed
youngyoung0 <taeyoung6873@gmail.com>
2023-04-19 13:47:12 +0900

    mvc2 api error - 스프링이 제공하는 ExceptionResolver2

--- spring/mvc2/exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ spring/mvc2/exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -4,9 +4,12 @@ import hello.exception.exception.BadRequestException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -28,6 +31,17 @@ public class ApiExceptionController {
     @GetMapping("api/response-status-ex1")
     public String responseStatusEx1(){
         throw new BadRequestException();
+    }
+
+    @GetMapping("api/response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new
+                IllegalArgumentException());
+    }
+
+    @GetMapping("api/response-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok";
     }
 
     @Data
