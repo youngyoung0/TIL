1ff9de58d539b863e4b1e86bde096e8f35d671c4
youngyoung0 <taeyoung6873@gmail.com>
2023-04-28 12:52:43 +0900

    jpa2 tuning - Project Initialization

---
+++ spring/jpa/src/main/java/hello/jpa/JpaMain.java
@@ -0,0 +1,13 @@
+package hello.jpa;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+    }
+
+}
