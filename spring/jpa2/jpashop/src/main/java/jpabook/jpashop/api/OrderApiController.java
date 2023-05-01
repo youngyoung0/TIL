cc446ce92061a5ed285b6d0ce8174272104c07cc
youngyoung0 <taeyoung6873@gmail.com>
2023-05-01 21:17:48 +0900

    jpa2 tuning - 컬렉셔 조회 최적화 : 페이징과 한계 돌파

--- spring/jpa2/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ spring/jpa2/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -10,6 +10,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -54,6 +55,20 @@ public class OrderApiController {
                 .collect(Collectors.toList());
         return collect;
     }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit
+            ) {
+        List<Order> orders = orderRepository.findAllWithItem(offset, limit);
+
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
 
     @Data
     static class OrderDto{
--- spring/jpa2/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ spring/jpa2/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -111,8 +111,16 @@ public class OrderRepository {
                         "join fetch o.delivery d " +
                         "join fetch o.orderItems oi " +
                         "join fetch oi.item i ", Order.class)
-                .setFirstResult(1)
-                .setMaxResults(100)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithItem(int offset, int limit){
+        return em.createQuery(
+                        "select distinct o from Order o " +
+                                "join fetch o.member m " +
+                                "join fetch o.delivery d ",Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
                 .getResultList();
     }
 }
