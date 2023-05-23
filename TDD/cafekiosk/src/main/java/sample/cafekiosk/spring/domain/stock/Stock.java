e30181165594fe2739695a7f2f5861fae9deeef6
youngyoung0 <taeyoung6873@gmail.com>
2023-05-24 01:55:39 +0900

    TDD - Business Layer 테스트 3 - 주문생성 시 재고 확인 및 개수 차감 후 생성하기 - 재고는 상품번호를 가진다. - 재과와 관련 있는 상품 타압은 병 음료, 베이커리

---
+++ TDD/cafekiosk/src/main/java/sample/cafekiosk/spring/domain/stock/Stock.java
@@ -0,0 +1,46 @@
+package sample.cafekiosk.spring.domain.stock;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productNumber;
+
+    private int quantity;
+
+    @Builder
+    private Stock(String productNumber, int quantity) {
+        this.productNumber = productNumber;
+        this.quantity = quantity;
+    }
+
+    public static Stock create(String productNumber, int quantity) {
+        return Stock.builder()
+                .productNumber(productNumber)
+                .quantity(quantity)
+                .build();
+    }
+
+    public boolean isQuantityLessThan(int quantity) {
+        return this.quantity < quantity;
+    }
+
+    public void deductQuantity(int quantity) {
+        if(isQuantityLessThan(quantity)){
+            throw new IllegalArgumentException("차감할 재고 수량이 없습니다.");
+        }
+        this.quantity -= quantity;
+    }
+}
