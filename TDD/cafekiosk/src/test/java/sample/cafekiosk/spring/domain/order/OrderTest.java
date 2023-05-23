567911d952e45f5c2672a6e2f2d5a1444fbcf594
youngyoung0 <taeyoung6873@gmail.com>
2023-05-23 15:37:27 +0900

    TDD - Business Layer 테스트 1

---
+++ TDD/cafekiosk/src/test/java/sample/cafekiosk/spring/domain/order/OrderTest.java
@@ -0,0 +1,79 @@
+package sample.cafekiosk.spring.domain.order;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
+
+@SpringBootTest
+class OrderTest {
+
+    @DisplayName("상품 리스트에서 주문의 총 금액을 계산한다.")
+    @Test
+    void calculateTotalPrice() {
+        // given
+        List<Product> products = List.of(
+                createProduct("001", 1000),
+                createProduct("002", 2000)
+        );
+
+        // when
+        Order order = Order.create(products, LocalDateTime.now());
+
+        // then
+        assertThat(order.getTotalPrice()).isEqualTo(3000);
+    }
+
+    @DisplayName("주문 생성시 주문 상태는 INIT이다")
+    @Test
+    void init() {
+        // given
+        List<Product> products = List.of(
+                createProduct("001", 1000),
+                createProduct("002", 2000)
+        );
+
+        // when
+        Order order = Order.create(products, LocalDateTime.now());
+
+        // then
+        assertThat(order.getOrderStatus()).isEqualByComparingTo(OrderStatus.INIT);
+    }
+
+    @DisplayName("주문 생성시 등록 시간을 기록한다.")
+    @Test
+    void registeredDateTime() {
+        // given
+        List<Product> products = List.of(
+                createProduct("001", 1000),
+                createProduct("002", 2000)
+        );
+
+        // when
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+        Order order = Order.create(products, registeredDateTime);
+
+        // then
+        assertThat(order.getRegisteredDateTime()).isEqualTo(registeredDateTime);
+    }
+
+    private Product createProduct(String productNumber, int price){
+        return Product.builder()
+                .type(ProductType.HANDMADE)
+                .productNumber(productNumber)
+                .price(price)
+                .sellingStatus(SELLING)
+                .name("메뉴 이름")
+                .build();
+    }
+
+}
