d8f13d8accb4dab557dcd2de0d24cd5ec2805153
youngyoung0 <taeyoung6873@gmail.com>
2023-05-23 13:48:07 +0900

    TDD - Persistence Layer 테스트 2

--- TDD/cafekiosk/src/main/java/sample/cafekiosk/spring/domain/product/Product.java
+++ TDD/cafekiosk/src/main/java/sample/cafekiosk/spring/domain/product/Product.java
@@ -1,6 +1,7 @@
 package sample.cafekiosk.spring.domain.product;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sample.cafekiosk.spring.domain.BaseEntity;
@@ -27,4 +28,14 @@ public class Product extends BaseEntity {
     private String name;
 
     private int price;
+
+    @Builder
+    public Product(Long id, String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.id = id;
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
 }
