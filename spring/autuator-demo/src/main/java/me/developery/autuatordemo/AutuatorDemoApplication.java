32dcd0a61877a9ba27fc442d5ee62bbf42aa3600
youngyoung0 <taeyoung6873@gmail.com>
2023-06-01 19:02:12 +0900

    actuator - project initializr

---
+++ spring/autuator-demo/src/main/java/me/developery/autuatordemo/AutuatorDemoApplication.java
@@ -0,0 +1,13 @@
+package me.developery.autuatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutuatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutuatorDemoApplication.class, args);
+	}
+
+}
