9a92a22dcefe776ba8b2c764735ee1f1c2ef0598
youngyoung0 <taeyoung6873@gmail.com>
2023-06-01 15:59:34 +0900

    TDD - Test fixture

--- TDD/cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
+++ TDD/cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
@@ -1,8 +1,6 @@
 package sample.cafekiosk.spring.api.service.product;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -33,6 +31,19 @@ class ProductServiceTest {
     @AfterEach
     void tearDown(){
         productRepository.deleteAllInBatch();
+    }
+
+    @BeforeAll
+    static void beforeAll(){
+        // before class
+    }
+
+    @BeforeEach
+    void setUp(){
+        // before method
+
+        // 각 테스트 입장에서 봤을 때 : 아예 몰라도 테스트 내용을 이해하는 데에 문제가 없는가?
+        // 수정해도 모든 테스트에 영향을 주지 않는가?
     }
 
     @DisplayName("신규 상품을 등록한다. 상품번호는 가장 최근 상품의 상품번호에서 1 증가한 값이다.")
