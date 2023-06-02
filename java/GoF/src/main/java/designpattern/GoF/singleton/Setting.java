43c240287306b75dccc68273e84348d96d7250d6
youngyoung0 <taeyoung6873@gmail.com>
2023-06-02 19:27:10 +0900

    GoF - singleton 멀티 쓰레드 환경에서 안전하게 구현하는 방법 2

--- java/GoF/src/main/java/designpattern/GoF/singleton/Setting.java
+++ java/GoF/src/main/java/designpattern/GoF/singleton/Setting.java
@@ -2,14 +2,40 @@ package designpattern.GoF.singleton;
 
 public class Setting {
 
+    // 이른 초기화 사용하기
+//    private static final Setting instace = new Setting();
+
     private static Setting instace;
 
     private Setting(){}
 
-    public static Setting getInstance(){
+    // sychronized 키워드 사용 동기화 체크 단, 성능에 문제가 있을수 있다.
+    public static synchronized Setting getInstance(){
         if(instace == null){
             instace = new Setting();
         }
         return instace;
+    }
+
+
+    // double chcked locking 사용하기
+    private static volatile Setting instace;
+    public static Setting getInstance(){
+        if(instace == null){
+            synchronized (Setting.class){
+                if(instace == null){
+                    instace = new Setting();
+                }
+            }
+        }
+    }
+
+    private static class SettingHolder{
+        private static final Setting instance = new Setting();
+    }
+
+    // static inner 클래스 사용하기
+    public static Setting getInstance(){
+        return SettingHolder.instance;
     }
 }
