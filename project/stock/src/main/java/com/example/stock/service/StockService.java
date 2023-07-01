215c5dd2696bfe33dfa1cd05a325469f6dfef42f
youngyoung0 <taeyoung6873@gmail.com>
2023-07-01 21:08:42 +0900

    stock - Synchronized 이용하면 발생하는 문제

--- project/stock/src/main/java/com/example/stock/service/StockService.java
+++ project/stock/src/main/java/com/example/stock/service/StockService.java
@@ -30,6 +30,10 @@ public class StockService {
          * synchronized와 @Transactional 같이 사용하면 문제점
          * Synchronized를 사용하는 이유는 해당 메서드를 한 쓰레드에서만 돌리기 위해서 이다.
          * 하지만, 트랜잭션이 같이 정의가 되어있다면 첫 번째 쓰레드가 끝나기 전 두 번째 쓰레드가 발동할 수도 있다.
+         *
+         * 문제점
+         * Synchronized 각 프로세스 안에서만 보장됩니다.
+         * 서버가 여러개 있을경우 사용할 수 없습니다.
          */
     }
 }
