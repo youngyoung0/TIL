b4c8190d93b3303ca7a8a58c80729eabaad12899
youngyoung0 <taeyoung6873@gmail.com>
2023-07-16 11:39:49 +0900

    querydsl - 스프링 데이터 JPA와 Querydsl

--- spring/querydsl/src/main/java/study/querydsl/controrller/MemberController.java
+++ spring/querydsl/src/main/java/study/querydsl/controrller/MemberController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
-import study.querydsl.entity.Member;
 import study.querydsl.repository.MemberJpaRepository;
 
 import java.util.List;
