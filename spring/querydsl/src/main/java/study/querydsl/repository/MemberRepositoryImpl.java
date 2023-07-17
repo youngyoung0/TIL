5a13afaf6fdd49b37f589f2d87a42c459d851f56
youngyoung0 <taeyoung6873@gmail.com>
2023-07-17 13:19:25 +0900

    querydsl - CountQuery 최적화

--- spring/querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ spring/querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -2,13 +2,16 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -102,7 +105,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                         ageLoe(condition.getAgeLoe()))
                 .fetchCount();
 
-        return new PageImpl<>(content, pageable, total);
+//        return new PageImpl<>(content, pageable, total);
 
         /**
          * 전체 카운트를 조회 하는 방법을 최적화 할 수 있으면 이렇게 분리하면 된다.
@@ -110,6 +113,24 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
          *
          * 코드를 리펙토링해서 내용 쿼리과 전체 카운트 쿼리를 읽기 좋게 분리하면 좋다.
          */
+
+        JPAQuery<Member> countQuery = queryFactory
+                .select(member)
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()));
+
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
+        /**
+         * 스르핑 데이터 라이브러리가 제공
+         * count 쿼리가 생략 가능한 경우 생략해서 처리
+         *  - 페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즤보다 작을때
+         *  - 마지막 페이지일때(offset + 컨텐츠 사이즈를 더해서 전체 사이즈 구함, 더 정학히는 마지막 페이지 이면서 컨텐츠 사이즈가 페이즈 사이즈보다 작을때)
+         */
+
     }
 
     private BooleanExpression usernameEq(String username) {
