97f2ae25fa8cb8443e66dab8c4895e66c76ffe74
youngyoung0 <taeyoung6873@gmail.com>
2023-07-21 00:38:45 +0900

    coupon : kafka Producer

--- project/coupon-system/api/src/main/java/com/example/api/service/ApplyService.java
+++ project/coupon-system/api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,6 @@
 package com.example.api.service;
 
-import com.example.api.entity.Coupon;
+import com.example.api.producer.CouponCreateProducer;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -11,20 +11,22 @@ public class ApplyService {
 
     private final CouponCountRepository couponCountRepository;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository){
+    private final CouponCreateProducer couponCreateProducer;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer){
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
+        this.couponCreateProducer = couponCreateProducer;
     }
 
     public void apply(Long userId){
         // redis incr key:value 싱글 스레드 기반으로 동작
         Long count = couponCountRepository.increment();
-
+        System.out.println(count);
         if(count > 100){
             return;
         }
-
-        couponRepository.save(new Coupon(userId));
+        couponCreateProducer.create(userId); // 토픽에 유저아이디를 전송
         /**
          * 싱글 스레드로 작업을 진행하게 된다면 레이스 컨디션이 발생하지 않습니다.
          * 하지만 1번사용자가 10:01에 쿠폰 발급을 완료하게 되면 다른 사용자는 10:01분 이후에 쿠폰 발급이 이뤄 지기 때문에 원하는 로직이랑 맞지 않습니다.
