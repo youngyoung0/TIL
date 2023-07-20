804792a5aa223d47b93f16d7f52386ad16f6b08a
youngyoung0 <taeyoung6873@gmail.com>
2023-07-20 21:23:36 +0900

    coupon : 쿠폰발급로직작성하기

--- project/coupon-system/api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ project/coupon-system/api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,7 +28,32 @@ class ApplyServiceTest {
         long count = couponRepository.count();
 
         assertThat(count).isEqualTo(1);
+    }
 
+    @Test
+    public void 여려명응모() throws InterruptedException {
+        int threadCount  = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount); // 다른 스레드에서 수행하는 작업을 기다리도록 도와주는 클래스
+
+        for(int i = 0; i< threadCount; i++){
+            long userId = i;
+            executorService.submit(() -> {
+                try{
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(100);
+        // 레이스 컨디션
+        // 두개 이상의 스레드가 공유 데이터에 엑세스로 하고 동시에 작업을 할때 발생하는 이슈
     }
 
 }
