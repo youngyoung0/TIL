f3754dc3e37773407bd8906182b2bf90b2fc7c08
youngyoung0 <taeyoung6873@gmail.com>
2023-08-13 01:32:22 +0900

    spring-tdd : h2를 이용한 repository 테스트

---
+++ TDD/scalableDevelopment/test-code-with-architecture-main/src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values(1, 'taeyoung6873@gmail.com', 'taeyoung', 'Seoul', 'aaaaa-aaaaa-aaaaaaa', 'ACTIVE', 0);
